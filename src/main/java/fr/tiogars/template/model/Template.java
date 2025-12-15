dc10ddf7575200525877949bf6f781d1a42570e2
tiogars <tiogars@gmail.com>
2025-12-15 21:06:37 +0100

    Create Template class with properties and constructor
    Initial creation of the Template class with fields and a default constructor.

---
+++ src/main/java/fr/tiogars/template/model/Template.java
@@ -0,0 +1,16 @@
+package fr.tiogars.template.model;
+
+public class Template {
+
+  private int id;
+
+  private String code;
+
+  private String name;
+
+  private String description;
+
+  public Template() {
+    // Default constructor
+  }
+}
